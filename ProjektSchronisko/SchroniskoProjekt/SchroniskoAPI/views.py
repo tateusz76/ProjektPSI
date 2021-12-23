440e3d64ecb5e6012f8173398deac137bc451d5c
Piotr <kowalowy1234@gmail.com>
2021-12-23 13:02:52 +0100

    Rozwiązanie problemów z polami wiele do wielu i jeden do wielu

--- ProjektSchronisko/SchroniskoProjekt/SchroniskoAPI/views.py
+++ ProjektSchronisko/SchroniskoProjekt/SchroniskoAPI/views.py
@@ -1,6 +1,8 @@
 from .models import Adopcja
 from .serializers import *
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class StanowiskoList(generics.ListAPIView):
@@ -11,6 +13,13 @@ class StanowiskoList(generics.ListAPIView):
 class RodzajeList(generics.ListAPIView):
     queryset = RodzajeZabiegow.objects.all()
     serializer_class = RodzajeSerializer
+    name = 'rodzaje-list'
+
+
+class RodzajeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RodzajeZabiegow.objects.all()
+    serializers_class = RodzajeSerializer
+    view_name = 'rodzaje-details'
 
 
 class AdopcjaList(generics.ListCreateAPIView):
@@ -26,11 +35,13 @@ class AdopcjaDetail(generics.RetrieveUpdateDestroyAPIView):
 class ZwierzeList(generics.ListCreateAPIView):
     queryset = Zwierze.objects.all()
     serializer_class = ZwierzeSerializer
+    name = 'zwierze-list'
 
 
 class ZwierzeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Zwierze.objects.all()
     serializer_class = ZwierzeSerializer
+    name = 'zwierze-details'
 
 
 class PracownikList(generics.ListCreateAPIView):
@@ -46,8 +57,23 @@ class PracownikDetail(generics.RetrieveUpdateDestroyAPIView):
 class ZabiegiList(generics.ListCreateAPIView):
     queryset = Zabieg.objects.all()
     serializer_class = ZabiegiSerializer
+    name = 'zabieg-list'
 
 
 class ZabiegiDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Zabieg.objects.all()
     serializer_class = ZabiegiSerializer
+    name = 'zabieg-details'
+
+
+class ApiRoot(generics.GenericAPIView):
+    name = 'api-root'
+
+    def get(self, request, *args, **kwargs):
+        return Response({'zwierzeta': reverse(ZwierzeList.name, request=request),
+                         'adopcje': reverse(AdopcjaList.name, request=request),
+                         'pracownicy': reverse(PracownikList.name, request=request),
+                         'zabiegi': reverse(ZabiegiList.name, request=request),
+                         'rodzajezabiegow': reverse(RodzajeList.name, request=request),
+                         'stanowiska': reverse(StanowiskoList.name, request=request)
+                         })
